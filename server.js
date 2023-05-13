3a559f200c0f542cd13c63198992362254f2911b
Gdel-J <jessygdelvecchio@gmail.com>
2023-05-13 13:44:19 -0400

    server.js file updated

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 //dependencies required
 const mysql2 = require("mysql2");
 const inquirer = require("inquirer");
-require("console.table");
 
+
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database is connected.');
+    Emp_tracker();
+});
